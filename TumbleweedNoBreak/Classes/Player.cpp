ee49ad96e8d5f6eba4b8a979c426290ce8206b27
Unknown <anthony.smiderle@uoit.net>
2019-02-12 21:19:24 -0500

    sprites

--- TumbleweedNoBreak/Classes/Player.cpp
+++ TumbleweedNoBreak/Classes/Player.cpp
@@ -7,7 +7,8 @@ namespace Sedna {
 	{
 		if (wPlayer == playerOne) {
 			sprite = cocos2d::Sprite::create("player1.png");
-			hitBox = new CirclePrimitive(cocos2d::Vec2(x, y), 20, 5, 30);
+			sprite->setScale(0.85f);
+			hitBox = new CirclePrimitive(cocos2d::Vec2(x, y), 26, 5, 30);
 
 			this->pController = X.getController(0);
 		
@@ -16,7 +17,8 @@ namespace Sedna {
 		else {
 
 			sprite = cocos2d::Sprite::create("player1.png");
-			hitBox = new CirclePrimitive(cocos2d::Vec2(x, y), 20, 5, 30);
+			sprite->setScale(0.85f);
+			hitBox = new CirclePrimitive(cocos2d::Vec2(x, y), 26, 5, 30);
 			this->pController = X.getController(1);
 
 		}
--- TumbleweedNoBreak/Classes/Table.cpp
+++ TumbleweedNoBreak/Classes/Table.cpp
@@ -3,7 +3,7 @@ namespace Sedna {
 
 Sedna::Table::Table(float x, float y)   {
 	this->sprite = cocos2d::Sprite::create("Table.png");
-	this->hitBox = new CirclePrimitive(cocos2d::Vec2(x, y),20,5,50);
+	this->hitBox = new CirclePrimitive(cocos2d::Vec2(x, y),26,5,50);
 	this->objectHp = 3;
 }
 void Sedna::Table::spriteSwitch()
